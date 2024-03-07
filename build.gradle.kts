40c01d64ff13989584a4d9a46e45a0bca831fc24
Michael Osterlie <github@outerrim.org>
2024-03-07 16:16:44 -0600

    Setting up mock obsidian library for testing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     kotlin("js") version "1.9.22"
-    kotlin("plugin.serialization") version "1.9.22"
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.22"
     id("io.kotest.multiplatform") version "5.8.0"
 }
 
