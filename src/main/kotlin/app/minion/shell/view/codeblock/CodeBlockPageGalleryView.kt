c1e3af98888d22c8b5a30409146d7b7cdf39a4e5
Michael Osterlie <github@outerrim.org>
2024-03-13 12:23:32 -0500

    Basic gallery view with action to change value

--- src/main/kotlin/app/minion/shell/view/codeblock/CodeBlockPageGalleryView.kt
+++ src/main/kotlin/app/minion/shell/view/codeblock/CodeBlockPageGalleryView.kt
@@ -1,15 +1,30 @@
 package app.minion.shell.view.codeblock
 
 import app.minion.core.MinionError
+import app.minion.core.functions.StatisticsFunctions.Companion.calculateTotalCount
+import app.minion.core.model.DataviewField
 import app.minion.core.model.FileData
 import app.minion.core.store.MinionStore
+import app.minion.shell.functions.VaultFunctions
 import app.minion.shell.view.codeblock.CodeBlockPageFunctions.Companion.applyCodeBlockConfig
 import app.minion.shell.view.codeblock.CodeBlockPageListView.Companion.addPageListView
+import app.minion.shell.view.codeblock.CodeBlockPageListView.Companion.outputFileDataSet
+import app.minion.shell.view.codeblock.CodeBlockPageListView.Companion.outputGroup
+import app.minion.shell.view.codeblock.CodeBlockPageListView.Companion.outputStats
+import app.minion.shell.view.iconHash
+import app.minion.shell.view.modal.UpdateDataviewValue
 import io.kvision.state.sub
 import kotlinx.dom.clear
 import kotlinx.html.FlowContent
 import kotlinx.html.div
 import kotlinx.html.dom.append
+import kotlinx.html.js.onClickFunction
+import kotlinx.html.label
+import kotlinx.html.li
+import kotlinx.html.span
+import kotlinx.html.title
+import kotlinx.html.ul
+import kotlinx.html.unsafe
 import mu.KotlinLogging
 import org.w3c.dom.HTMLElement
 
@@ -45,13 +60,67 @@ interface CodeBlockPageGalleryView { companion object {
                 +config.heading
             }
         }
+
+        if (fileDataMap.isNotEmpty()) {
+            element.append.div {
+                fileDataMap.forEach { entry ->
+                    outputGroup(config, entry.key, entry.value, store)
+                }
+            }
+        }
+
+        element.outputStats(fileDataMap)
     }
 
-    fun FlowContent.outputGroup(config: CodeBlockConfig, label: String, fileDataMap: Map<String, Set<FileData>>, store: MinionStore) {
-
+    fun FlowContent.outputGroup(config: CodeBlockConfig, label: String, fileDataSet: Set<FileData>, store: MinionStore) {
+        if (label == GROUP_BY_SINGLE) {
+            outputFileDataSet(config, fileDataSet, store)
+        } else {
+            div {
+                div(classes = "mi-codeblock-group-label") { +label }
+                outputFileDataSet(config, fileDataSet, store)
+            }
+        }
     }
 
     fun FlowContent.outputFileDataSet(config: CodeBlockConfig, fileDataSet: Set<FileData>, store: MinionStore) {
+        div(classes = "mi-codeblock-page-gallery") {
+            fileDataSet.forEach { fileData ->
+                outputFileData(fileData, config, store)
+            }
+        }
+    }
 
+    fun FlowContent.outputFileData(fileData: FileData, config: CodeBlockConfig, store: MinionStore) {
+        div {
+            div(classes = "mi-codeblock-page-gallery-title") {
+                span(classes = "mi-codeblock-source-link") {
+                    +fileData.name.v
+                    onClickFunction = {
+                        VaultFunctions.openSourceFile(fileData.name, store.store.state.plugin.app)
+                    }
+                }
+                span(classes = "mi-icon mi-button") {
+                    title = "Change group value"
+                    unsafe { +iconHash }
+                    onClickFunction = {
+                        UpdateDataviewValue(
+                            fileData,
+                            config.groupByField,
+                            fileData.dataview[DataviewField(config.groupByField)]!!,
+                            store.store.state.dataviewValueCache[DataviewField(config.groupByField)]!!,
+                            store,
+                            store.store.state.plugin.app
+                        ).open()
+                    }
+                }
+            }
+        }
+    }
+
+    fun HTMLElement.outputStats(fileDataList: Map<String, Set<FileData>>) {
+        this.append.div(classes = "mi-codeblock-item-count") {
+            +"Page Count: ${fileDataList.calculateTotalCount()}"
+        }
     }
 }}
