26ceb714763f29e11eae8a1d1ece2f756146631d
Michael Osterlie <github@outerrim.org>
2024-03-14 09:17:17 -0500

    Created updated settings for excluding a folder from processing

--- src/main/kotlin/app/minion/core/functions/SettingsFunctions.kt
+++ src/main/kotlin/app/minion/core/functions/SettingsFunctions.kt
@@ -2,6 +2,7 @@ package app.minion.core.functions
 
 import app.minion.core.MinionError
 import app.minion.core.model.MinionSettings1
+import app.minion.core.model.MinionSettings2
 import app.minion.core.model.SettingsVersion
 import arrow.core.Either
 import arrow.core.Option
@@ -17,23 +18,26 @@ interface SettingsFunctions { companion object {
     private val jsonSerializer = Json { ignoreUnknownKeys = true }
 
     fun loadFromJson(maybeJson: Option<String>)
-    : Either<MinionError, MinionSettings1> = either {
+    : Either<MinionError, MinionSettings2> = either {
         logger.debug { "loadFromJson" }
         maybeJson.fold(
             ifEmpty = {
-                MinionSettings1.default()
+                MinionSettings2.default()
             },
             ifSome = { json ->
                 runCatching {
                     when(jsonSerializer.decodeFromString<SettingsVersion>(json).version) {
+                        "2" -> {
+                            logger.debug { "Version 2, just loading" }
+                            jsonSerializer.decodeFromString<MinionSettings2>(json)
+                        }
                         "1" -> {
-                            logger.debug { "Version 1, just loading" }
+                            logger.debug { "Version 1, upgrading" }
                             jsonSerializer
                                 .decodeFromString<MinionSettings1>(json)
+                                .upgradeTo2()
                         }
-                        else -> {
-                            raise(MinionError.LoadSettingsError("Cannot load settings for JSON: $json"))
-                        }
+                        else -> raise(MinionError.LoadSettingsError("Cannot load settings for JSON: $json"))
                     }
                 }
                     .getOrElse {
@@ -42,6 +46,13 @@ interface SettingsFunctions { companion object {
                         )
                     }
            }
+        )
+    }
+
+    fun MinionSettings1.upgradeTo2() : MinionSettings2 {
+        logger.debug { "MinionSettings1.upgradeTo2()" }
+        return MinionSettings2.default().copy(
+            lifeAreas = this.lifeAreas
         )
     }
 
